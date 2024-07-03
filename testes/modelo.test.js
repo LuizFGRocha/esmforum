f2ecc1fb1c2136c623289d477a36ff3657b067bf
Luiz Fernando <luizfgrocha04@gmail.com>
2024-07-03 01:11:24 -0300

    test: Criados testes de respostas e de get pegunta. Cobertura 100%

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,27 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de três respostas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  let id_pergunta = modelo.listar_perguntas()[0].id_pergunta;
+
+  modelo.cadastrar_resposta(id_pergunta, '2');
+  modelo.cadastrar_resposta(id_pergunta, '3');
+  modelo.cadastrar_resposta(id_pergunta, '4');
+
+  const respostas = modelo.get_respostas(id_pergunta); 
+
+  expect(respostas.length).toBe(3);
+  expect(respostas[0].texto).toBe('2');
+  expect(respostas[1].texto).toBe('3');
+  expect(respostas[2].texto).toBe('4');
+  expect(respostas[1].id_resposta).toBe(respostas[2].id_resposta-1);
+});
+
+test('Testando get_pergunta', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  let id_pergunta = modelo.listar_perguntas()[0].id_pergunta;
+  const pergunta = modelo.get_pergunta(id_pergunta);
+  expect(pergunta.texto).toBe('1 + 1 = ?');
+});
